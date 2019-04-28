e78f3c16fe8e9405044a477dfbbbc6b4770118ef
Ralph Vente <venteralph@gmail.com>
2019-04-27 23:49:44 -0400

    add updates to configs

--- dotconfig/chromium/Default/Extensions/hdokiejnpimakedhajhdlcegeplioahd/4.9.1.0_0/modaloverlay/removeModalOverlay.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.body;if(e){var o=document.getElementById("lpiframeoverlaymodal");o&&e.removeChild(o)}}();
-//# sourceMappingURL=sourcemaps/modaloverlay/removeModalOverlay.js.map
